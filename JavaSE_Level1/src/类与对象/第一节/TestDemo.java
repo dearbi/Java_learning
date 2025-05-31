bee29e53e4902d80935e604fef122c51f2d3df53
dear_bi <2199463351@qq.com>
2025-05-31 15:25:30 +0800

    commit

--- JavaSE_Level1/src/类与对象/第一节/TestDemo.java
+++ JavaSE_Level1/src/类与对象/第一节/TestDemo.java
@@ -4,6 +4,10 @@ public class TestDemo {
     public static void main(String[] args) {
         int a = 10;
         Dog dog1 = new Dog();
+        dog1.name="旺财";
+        dog1.age=10;
+        dog1.brand="金毛";
+        System.out.println(dog1.name);
         Dog dog2 = new Dog();
         Dog dog3 = new Dog();
     }
--- JavaSE_Level1/src/类与对象/第一节/第一节20250525.java
+++
@@ -1,7 +0,0 @@
-package 类与对象.第一节;
-
-public class 第一节20250525 {
-    public static void main(String[] args) {
-
-    }
-}
