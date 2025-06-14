95a0ca260bc4063fa72855120d620c802400f2b7
dear_bi <2199463351@qq.com>
2025-06-14 14:20:52 +0800

    测试用例

--- JavaSE_Level1/src/Main.java
+++ JavaSE_Level1/src/Main.java
@@ -1,55 +1,29 @@
-import java.util.Scanner;
+import java.util.*;
 
 public class Main {
-
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        while (scanner.hasNextInt()) {
-            int x = scanner.nextInt();
-            int y = scanner.nextInt();
-            int z = scanner.nextInt();
-            Sub sub = new Sub(x, y, z);
-            System.out.println(sub.calculate());
+        Scanner cin = new Scanner(System.in);
+        int t1 = cin.nextInt();
+        if (t1 < 0 || t1 > 12) {
+            System.out.println("Value for hours must be in the range 0 to 12");
+            return;
         }
+        int t2 = cin.nextInt();
+        if (t2 < 0 || t2 > 59) {
+            System.out.println("Value for minutes must be in the range 0 to 59");
+            return;
+        } else {
+            int c = cin.nextInt();
+            if (c == 1) {
+                if (t1 >= 12) {
+                    t1 = t1 % 12;
+                }
+            } else {
+                if (t1 == 0) {
+                    t1 = 12;
+                }
+            }
+        }
+        System.out.printf("%d:%02d", t1, t2);
     }
-
-}
-
-class Base {
-
-    private int x;
-    private int y;
-
-    public Base(int x, int y) {
-        this.x = x;
-        this.y = y;
-    }
-
-    public int getX() {
-        return x;
-    }
-
-    public int getY() {
-        return y;
-    }
-
-}
-
-class Sub extends Base {
-
-    private int z;
-
-    public Sub(int x, int y, int z) {
-        super(x, y);
-        this.z = z;
-    }
-
-    public int getZ() {
-        return z;
-    }
-
-    public int calculate() {
-        return super.getX() * super.getY() * this.getZ();
-    }
-
 }
