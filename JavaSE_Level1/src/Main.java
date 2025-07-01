137445239e824907fbbef49642d6c5f5707c54b6
bwb <2199463351@qq.com>
2025-07-01 15:38:30 +0800

    测试

--- JavaSE_Level1/src/Main.java
+++ JavaSE_Level1/src/Main.java
@@ -1,21 +1,11 @@
-import java.util.*;
-
-class Main {
+public class Main {
     public static void main(String[] args) {
-        Scanner cin = new Scanner(System.in);
-        String s = cin.nextLine();
-        int n = s.length();
-        if (s.charAt(n - 1) != 'y') {
-            System.out.println("Cannot form the plural form with the rules given.");
-        } else {
-            if (s.charAt(n - 2) == 'u' || s.charAt(n - 3) == 'q') {
-                System.out.print("soliloquy in plural form is: ");
-                System.out.println(s.substring(0, n - 1) + "ies");
-            } else if ("qeiou".indexOf(s.charAt(n - 2)) != -1) {
-
-                System.out.print(s + " ");
-                System.out.print("in plural form is: " + s + "s");
-            }
-        }
+        int[][] values = {{3, 4, 5, 1}, {33, 6, 1, 2}};
+        int v = values[0][0];
+        for (int[] list : values) //for-each循环，主要用于访问整个数组
+            for (int element : list)
+                if (v > element)
+                    v = element;
+        System.out.print(v);
     }
 }
