43be2782be90bc6bcd7cf7fc0a4ee2586dd07283
bwb <2199463351@qq.com>
2025-07-02 11:17:04 +0800

    git测试

--- JavaSE_Level1/src/Main.java
+++ JavaSE_Level1/src/Main.java
@@ -1,11 +1,26 @@
+import java.util.*;
+
 public class Main {
     public static void main(String[] args) {
-        int[][] values = {{3, 4, 5, 1}, {33, 6, 1, 2}};
-        int v = values[0][0];
-        for (int[] list : values) //for-each循环，主要用于访问整个数组
-            for (int element : list)
-                if (v > element)
-                    v = element;
-        System.out.print(v);
+        Scanner cin = new Scanner(System.in);
+        int n = cin.nextInt();
+        int[] a = new int[101];
+        for (int i = 1; i <= n; i++) {
+            a[i] = cin.nextInt();
+            for (int j = i - 1; j >= 0; j--) {
+                if (a[j] > a[j + 1]) {
+                    int temp = a[j];
+                    a[j] = a[j + 1];
+                    a[j + 1] = temp;
+                }
+            }
+            for (int j = 1; j <= i; j++) {
+                if (i == j) {
+                    System.out.println(a[j] + " ");
+                } else {
+                    System.out.print(a[j] + " ");
+                }
+            }
+        }
     }
 }
