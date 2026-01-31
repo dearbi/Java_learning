194939043f81bf49508ac302ccbb5a2d61885f0d
dearbi <2199463351@qq.com>
2026-01-31 21:09:36 +0800

    a

--- JavaEE/src/Main.java
+++ JavaEE/src/Main.java
@@ -1,5 +1,19 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        Thread t=new Thread(()->{
+           for (int i = 0; i < 10; i++) {
+
+           }
+
+        }, "子线程");
+        System.out.println(t.getName()+":"+t.getState());
+        t.start();
+        System.out.println(t.getName()+":"+t.getState());
+        while(t.isAlive()){
+            System.out.println(t.getName()+":"+t.getState());
+        }
+        System.out.println(t.getName()+":"+t.getState());
+
     }
+
 }
