74aacc0eb7c7fb3d0e3db7e9b204664f31d5c62f
dearbi <2199463351@qq.com>
2026-07-17 21:28:07 +0800

    留言板demo

--- SpringMVC_demo1/src/main/java/com/wenbobi/springmvc_demo1/MessageController.java
+++ SpringMVC_demo1/src/main/java/com/wenbobi/springmvc_demo1/MessageController.java
@@ -1,18 +1,33 @@
 package com.wenbobi.springmvc_demo1;
 
 
+import org.springframework.util.StringUtils;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @RestController
 @RequestMapping("/message")
 public class MessageController {
-    @RequestMapping("/test")
-    public String test() {
-        MessageInfo messageInfo = new MessageInfo();
-        messageInfo.setFrom("admin");
-        messageInfo.setTo("user");
-        messageInfo.setMessage("hello user");
-        return messageInfo.toString();
+    List<MessageInfo> messageInfoList=new ArrayList<>();
+
+    @RequestMapping("/getList")
+    public List<MessageInfo> getList() {
+        return messageInfoList;
+    }
+
+    @RequestMapping(value = "/publish",produces = "application/json")
+       public String publish(@RequestBody MessageInfo messageInfo){
+
+        //参数校验
+        if(!StringUtils.hasText(messageInfo.getFrom()) || !StringUtils.hasText(messageInfo.getTo()) ||
+                !StringUtils.hasText(messageInfo.getMessage())){
+            return "{\"ok\":0}";
+        }
+        messageInfoList.add(messageInfo);
+        return "{\"ok\":1}";
     }
 }
--- SpringMVC_demo1/src/main/java/com/wenbobi/springmvc_demo1/MessageInfo.java
+++ SpringMVC_demo1/src/main/java/com/wenbobi/springmvc_demo1/MessageInfo.java
@@ -1,10 +1,10 @@
 package com.wenbobi.springmvc_demo1;
 
-import lombok.Getter;
-import lombok.Setter;
+import lombok.*;
 
-@Getter
-@Setter
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class MessageInfo {
     private String from;
     private String to;
